0aec1f853cd44be704768ead3bd63ea07c870cf9
Nazmul <Nazmul.abbu@gmail.com>
2022-03-04 00:21:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./Routes/Landing";
+import Notfound from "./Routes/Notfound";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+import Switch from "@mui/material/Switch";
+
+const dark = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#ff0",
+            light: "#744",
+            dark: "#000",
+        },
+    },
+});
+const light = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#f00",
+            light: "#744",
+            dark: "#000",
+        },
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+    const changeTheme = () => {
+        setIsDarkTheme(!isDarkTheme);
+    };
+    return (
+        <>
+            <ThemeProvider theme={isDarkTheme ? dark : light}>
+                <Switch checked={isDarkTheme} onChange={changeTheme} />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Landing />} />
+                        <Route path="*" element={<Notfound />} />
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+function Hero() {
+    return (
+        <>
+            <Typography color={"primary"}>Hello World</Typography>
+        </>
+    );
+}
+
+export default Hero;
